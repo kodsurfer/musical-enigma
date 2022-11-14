0aeed296ea05df96cea07bb18f23dce30c4a0316
kodsurfer <kodsurfer@ya.ru>
2022-11-15 02:29:50 +0300

    add window

--- main.go
+++ main.go
@@ -10,5 +10,15 @@ func main() {
 	gtk.Init(nil)
 	log.Print("Start musical-enigma")
 
-	
+	window, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatalf("Create window error %s", err)
+	}
+
+	window.SetTitle("Musical Enigma MP3 Player")
+
+	window.Connect("quit", func() {
+		gtk.MainQuit()
+	})
+
 }
