62eb7e9862bd9f1c6d70c5f90eefbaa968c8f54d
kodsurfer <kodsurfer@ya.ru>
2022-11-15 02:15:55 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module musical-enigma
+
+go 1.18
+
+require github.com/gotk3/gotk3 v0.6.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gotk3/gotk3/gtk"
+)
+
+func main() {
+	gtk.Init(nil)
+	log.Print("Start musical-enigma")
+
+	
+}
