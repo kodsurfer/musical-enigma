9d2ee16ccae18f76821407c0f000c6c5af4dc354
kodsurfer <kodsurfer@ya.ru>
2022-11-16 02:11:19 +0300

    add id3 tag parse

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module musical-enigma
 
 go 1.18
 
-require github.com/gotk3/gotk3 v0.6.1
+require (
+	github.com/bogem/id3v2/v2 v2.1.3
+	github.com/gotk3/gotk3 v0.6.1
+)
+
+require golang.org/x/text v0.3.2 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 
+	"os"
+
+	"github.com/bogem/id3v2/v2"
 	"github.com/gotk3/gotk3/gtk"
 )
 
@@ -22,12 +25,19 @@ func main() {
 	})
 
 	userInterface(window)
+
+	mp3 := os.Args[1]
+	_, err = id3v2.Open(mp3, id3v2.Options{Parse: true})
+	if err != nil {
+		log.Fatal("id3 tag parse error")
+	}
+
 }
 
 func userInterface(window *gtk.Window) {
 	btn, _ := gtk.ButtonNew()
 	btn.Connect("click", func() {
-		//
+		//click state
 	})
 
 	btn_label, _ := gtk.LabelNew("Play/Pause")
